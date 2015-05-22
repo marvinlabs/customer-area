cfc924a3b786c40c5288ffe3f06f74c2b7debfe7
Thomas Lartaud <contact@thomaslartaud.com>
2015-05-22 15:42:08 +0200

    add some panels for dashboard

---
+++ skins/frontend/master/assets/js/main.js
@@ -0,0 +1,62 @@
+;(function($, window, undefined)
+{
+    "use strict";
+
+    $(document).ready(function()
+    {
+        // Panel Close
+
+        $('body').on('click', '.cuar-panel a[data-toggle="remove"]', function(ev)
+        {
+            ev.preventDefault();
+
+            var $panel = $(this).closest('.cuar-panel'),
+                $panel_parent = $panel.parent();
+
+            $panel.remove();
+
+            if($panel_parent.children().length == 0)
+            {
+                $panel_parent.remove();
+            }
+        });
+
+
+
+        // Panel Reload
+        $('body').on('click', '.cuar-panel a[data-toggle="reload"]', function(ev)
+        {
+            ev.preventDefault();
+
+            var $panel = $(this).closest('.cuar-panel');
+
+            // This is just a simulation, nothing is going to be reloaded
+            $panel.append('<div class="cuar-panel-disabled"><div class="loader-1"></div></div>');
+
+            var $pd = $panel.find('.cuar-panel-disabled');
+
+            setTimeout(function()
+            {
+                $pd.fadeOut('fast', function()
+                {
+                    $pd.remove();
+                });
+
+            }, 500 + 300 * (Math.random() * 5));
+        });
+
+
+
+        // Panel Expand/Collapse Toggle
+        $('.cuar-panel').on('click', 'a[data-toggle="panel"]', function(ev)
+        {
+            ev.preventDefault();
+
+            var $panel = $(this).closest('.cuar-panel');
+
+            $panel.toggleClass('cuar-collapsed');
+        });
+
+    });
+
+})(jQuery, window);
