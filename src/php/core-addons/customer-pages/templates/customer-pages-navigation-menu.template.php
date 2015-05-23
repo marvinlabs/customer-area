3a8b6095b299c7b972196440b99969436ba0475f
Thomas Lartaud <contact@thomaslartaud.com>
2015-05-23 14:52:05 +0200

    switch users template improve

--- src/php/core-addons/customer-pages/templates/customer-pages-navigation-menu.template.php
+++ src/php/core-addons/customer-pages/templates/customer-pages-navigation-menu.template.php
@@ -11,19 +11,16 @@
 ?>
 
     <nav class="cuar-navbar cuar-navbar-default" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="cuar-container">
-            <div class="cuar-navbar-header">
-                <button type="button" class="cuar-navbar-toggle" data-toggle="collapse"
-                        data-target=".cuar-nav-container">
-                    <span class="cuar-sr-only"><?php __('Toggle navigation', 'cuar'); ?></span>
-                    <span class="cuar-icon-bar"></span>
-                    <span class="cuar-icon-bar"></span>
-                    <span class="cuar-icon-bar"></span>
-                </button>
-            </div>
-            <?php wp_nav_menu($nav_menu_args); ?>
+        <div class="cuar-navbar-header">
+            <button type="button" class="cuar-navbar-toggle" data-toggle="collapse"
+                    data-target=".cuar-nav-container">
+                <span class="cuar-sr-only"><?php __('Toggle navigation', 'cuar'); ?></span>
+                <span class="cuar-icon-bar"></span>
+                <span class="cuar-icon-bar"></span>
+                <span class="cuar-icon-bar"></span>
+            </button>
         </div>
+        <?php wp_nav_menu($nav_menu_args); ?>
     </nav>
 
 <?php echo $this->get_subpages_menu(); ?>
