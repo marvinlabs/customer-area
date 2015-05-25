17a889d1cf9b3fcd34cc4c754a6e6cc3453f7956
Thomas Lartaud <contact@thomaslartaud.com>
2015-05-25 18:15:04 +0200

    fork / move wp_bootstrap_navwalker outside of vendors

--- skins/frontend/master/cuar-functions.php
+++ skins/frontend/master/cuar-functions.php
@@ -34,15 +34,15 @@ if ( !function_exists( 'cuar_enable_bootstrap_nav_walker' ) ) {
 	 * Use the bootstrap navwalker for our navigation menu to output bootstrap-friendly HTML.
 	 */
 	function cuar_enable_bootstrap_nav_walker( $args ) {
-		require_once( CUAR_PLUGIN_DIR . '/libs/wp-bootstrap-navwalker/wp_bootstrap_navwalker.php' );		
+        require_once( CUAR_PLUGIN_DIR . '/src/php/helpers/bootstrap-nav-walker.class.php' );
 		$new_args = $args;
 		
 		$new_args['depth'] = 2;
 		$new_args['container'] = 'div';
 		$new_args['container_class'] = 'cuar-nav-container cuar-collapse cuar-navbar-collapse';
 		$new_args['menu_class'] = 'cuar-nav cuar-navbar';
-		$new_args['fallback_cb'] = 'wp_bootstrap_navwalker::fallback';
-		$new_args['walker'] = new wp_bootstrap_navwalker();		
+		$new_args['fallback_cb'] = 'CUAR_BootstrapNavWalker::fallback';
+		$new_args['walker'] = new CUAR_BootstrapNavWalker();
 		
 		return $new_args;
 	}
