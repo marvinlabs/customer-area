706b637b39e01812880d8d7ee4c78b440dc2dcbd
Vincent Mimoun-Prat <vpratfr@gmail.com>
2015-07-10 19:52:12 +0200

    Formatting & use function cuar() instead of CUAR_Plugin::get_instance()

--- skins/frontend/master/cuar-functions.php
+++ skins/frontend/master/cuar-functions.php
@@ -1,14 +1,16 @@
-<?php 
+<?php
 
-if ( !function_exists( 'cuar_load_theme_scripts' ) ) {
-	
-	/**
-	 * Load theme particular scripts
-	 */
-	function cuar_load_skin_scripts() {
+if ( !function_exists('cuar_load_theme_scripts'))
+{
 
-        if ( cuar_is_customer_area_page(get_queried_object_id()) || cuar_is_customer_area_private_content(get_the_ID()) ) {
-            $cuar_plugin = CUAR_Plugin::get_instance();
+    /**
+     * Load theme particular scripts
+     */
+    function cuar_load_skin_scripts()
+    {
+        if (cuar_is_customer_area_page(get_queried_object_id()) || cuar_is_customer_area_private_content(get_the_ID()))
+        {
+            $cuar_plugin = cuar();
 
             $cuar_plugin->enable_library('bootstrap.affix');
             $cuar_plugin->enable_library('bootstrap.alert');
@@ -25,34 +27,46 @@ if ( !function_exists( 'cuar_load_theme_scripts' ) ) {
 
             $cuar_plugin->enable_library('jquery.mixitup');
 
-            wp_register_script('customer-area-utilities', CUAR_PLUGIN_URL . '/assets/frontend/js/customer-area.min.js', array('jquery', 'jquery-ui-core', 'jquery-ui-draggable', 'jquery-ui-droppable', 'jquery-ui-sortable'), $cuar_plugin->get_version(), true);
-            wp_register_script('customer-area-master-skin', CUAR_PLUGIN_URL . '/skins/frontend/master/assets/js/main.js', array('jquery', 'customer-area-utilities'), $cuar_plugin->get_version(), true);
+            wp_register_script('customer-area-utilities',
+                CUAR_PLUGIN_URL . '/assets/frontend/js/customer-area.min.js',
+                array('jquery', 'jquery-ui-core', 'jquery-ui-draggable', 'jquery-ui-droppable', 'jquery-ui-sortable'),
+                $cuar_plugin->get_version(),
+                true);
+
+            wp_register_script('customer-area-master-skin',
+                CUAR_PLUGIN_URL . '/skins/frontend/master/assets/js/main.js',
+                array('jquery', 'customer-area-utilities'),
+                $cuar_plugin->get_version(),
+                true);
+
             wp_enqueue_script('customer-area-master-skin');
         }
-
     }
-	add_action('wp_enqueue_scripts', 'cuar_load_skin_scripts');
+
+    add_action('wp_enqueue_scripts', 'cuar_load_skin_scripts');
 }
 
 
+if ( !function_exists('cuar_enable_bootstrap_nav_walker'))
+{
 
-if ( !function_exists( 'cuar_enable_bootstrap_nav_walker' ) ) {
-	
-	/**
-	 * Use the bootstrap navwalker for our navigation menu to output bootstrap-friendly HTML.
-	 */
-	function cuar_enable_bootstrap_nav_walker( $args ) {
-        require_once( CUAR_PLUGIN_DIR . '/src/php/helpers/bootstrap-nav-walker.class.php' );
-		$new_args = $args;
-		
-		$new_args['depth'] = 2;
-		$new_args['container'] = 'div';
-		$new_args['container_class'] = 'cuar-nav-container cuar-collapse cuar-navbar-collapse';
-		$new_args['menu_class'] = 'cuar-nav cuar-navbar';
-		$new_args['fallback_cb'] = 'CUAR_BootstrapNavWalker::fallback';
-		$new_args['walker'] = new CUAR_BootstrapNavWalker();
-		
-		return $new_args;
-	}
-	add_filter( 'cuar/core/page/nav-menu-args', 'cuar_enable_bootstrap_nav_walker' );
+    /**
+     * Use the bootstrap navwalker for our navigation menu to output bootstrap-friendly HTML.
+     */
+    function cuar_enable_bootstrap_nav_walker($args)
+    {
+        require_once(CUAR_PLUGIN_DIR . '/src/php/helpers/bootstrap-nav-walker.class.php');
+        $new_args = $args;
+
+        $new_args['depth'] = 2;
+        $new_args['container'] = 'div';
+        $new_args['container_class'] = 'cuar-nav-container cuar-collapse cuar-navbar-collapse';
+        $new_args['menu_class'] = 'cuar-nav cuar-navbar';
+        $new_args['fallback_cb'] = 'CUAR_BootstrapNavWalker::fallback';
+        $new_args['walker'] = new CUAR_BootstrapNavWalker();
+
+        return $new_args;
+    }
+
+    add_filter('cuar/core/page/nav-menu-args', 'cuar_enable_bootstrap_nav_walker');
 }
