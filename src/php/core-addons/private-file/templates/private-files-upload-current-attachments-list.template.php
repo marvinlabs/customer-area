9feddb5f7a67d8b05d9e7cef6e8d26a3cba01080
Vincent Mimoun-Prat <vpratfr@gmail.com>
2015-07-11 17:14:28 +0200

    Add templates for the metabox

---
+++ src/php/core-addons/private-file/templates/private-files-upload-current-attachments-list.template.php
@@ -0,0 +1,12 @@
+<?php /** Template version: 1.0.0
+
+ */
+
+/** @var array $attached_files */
+?>
+
+<div class="cuar-private-file-attachments">
+<?php foreach ($attached_files as $attached_file) : ?>
+
+<?php endforeach; ?>
+</div>
---
+++ src/php/core-addons/private-file/templates/private-files-upload-select-files.template.php
@@ -0,0 +1,8 @@
+<?php /** Template version: 1.0.0
+
+ */
+
+?>
+
+<div class="cuar-private-file-upload-select">
+</div>
