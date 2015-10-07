e4b56cd73cc211ce42a280d12e7ec0b0fd365dc8
Vincent Mimoun-Prat <vpratfr@gmail.com>
2015-10-07 17:55:13 +0200

    Remove plurals

--- src/php/helpers/currency-helper.class.php
+++ src/php/helpers/currency-helper.class.php
@@ -33,33 +33,33 @@ if ( !class_exists('CUAR_CurrencyHelper')) :
         public static function getCurrencies()
         {
             return apply_filters('cuar/core/currencies', array(
-                'AUD'  => __( 'Australian Dollars (&#36;)', 'cuar' ),
+                'AUD'  => __( 'Australian Dollar (&#36;)', 'cuar' ),
                 'BRL'  => __( 'Brazilian Real (R&#36;)', 'cuar' ),
-                'CAD'  => __( 'Canadian Dollars (&#36;)', 'cuar' ),
+                'CAD'  => __( 'Canadian Dollar (&#36;)', 'cuar' ),
                 'CZK'  => __( 'Czech Koruna', 'cuar' ),
                 'DKK'  => __( 'Danish Krone', 'cuar' ),
-                'EUR'  => __( 'Euros (&euro;)', 'cuar' ),
+                'EUR'  => __( 'Euro (&euro;)', 'cuar' ),
                 'HKD'  => __( 'Hong Kong Dollar (&#36;)', 'cuar' ),
                 'HUF'  => __( 'Hungarian Forint', 'cuar' ),
                 'INR'  => __( 'Indian Rupee (&#8377;)', 'cuar' ),
                 'RIAL' => __( 'Iranian Rial (&#65020;)', 'cuar' ),
                 'ILS'  => __( 'Israeli Shekel (&#8362;)', 'cuar' ),
                 'JPY'  => __( 'Japanese Yen (&yen;)', 'cuar' ),
-                'MYR'  => __( 'Malaysian Ringgits', 'cuar' ),
+                'MYR'  => __( 'Malaysian Ringgit', 'cuar' ),
                 'MXN'  => __( 'Mexican Peso (&#36;)', 'cuar' ),
                 'NZD'  => __( 'New Zealand Dollar (&#36;)', 'cuar' ),
                 'NOK'  => __( 'Norwegian Krone', 'cuar' ),
-                'PHP'  => __( 'Philippine Pesos', 'cuar' ),
+                'PHP'  => __( 'Philippine Peso', 'cuar' ),
                 'PLN'  => __( 'Polish Zloty', 'cuar' ),
                 'GBP'  => __( 'Pounds Sterling (&pound;)', 'cuar' ),
-                'RUB'  => __( 'Russian Rubles', 'cuar' ),
+                'RUB'  => __( 'Russian Ruble', 'cuar' ),
                 'SGD'  => __( 'Singapore Dollar (&#36;)', 'cuar' ),
                 'SEK'  => __( 'Swedish Krona', 'cuar' ),
                 'CHF'  => __( 'Swiss Franc', 'cuar' ),
-                'TWD'  => __( 'Taiwan New Dollars', 'cuar' ),
+                'TWD'  => __( 'Taiwan New Dollar', 'cuar' ),
                 'THB'  => __( 'Thai Baht (&#3647;)', 'cuar' ),
                 'TRY'  => __( 'Turkish Lira (&#8378;)', 'cuar' ),
-                'USD'  => __( 'US Dollars (&#36;)', 'cuar' )
+                'USD'  => __( 'US Dollar (&#36;)', 'cuar' )
             ));
         }
 
