e034f823fa8ff8ac1b7c4384cd519593be6c83ae
Thomas Lartaud <contact@thomaslartaud.com>
2016-01-05 16:44:21 +0100

    default wpca page template

--- src/php/core-classes/templates/customer-page.template.php
+++ src/php/core-classes/templates/customer-page.template.php
@@ -1,28 +1,36 @@
 <?php /** Template version: 1.0.0 */ ?>
 
 <?php
-	$page_classes = array( 'cuar-page-' . $this->page_description['slug'] );
-	if ( $this->has_page_sidebar() ) $page_classes[] = "cuar-page-with-sidebar";
-	else $page_classes[] = "cuar-page-without-sidebar";
+$page_classes = array('cuar-page-' . $this->page_description['slug']);
+if ($this->has_page_sidebar()) $page_classes[] = "cuar-page-with-sidebar";
+else $page_classes[] = "cuar-page-without-sidebar";
 
-	$content_classe = $this->page_description['slug'] == 'customer-dashboard' ? ' admin-panels' : '';
+$content_class = $this->page_description['slug'] == 'customer-dashboard' ? ' cuar-customer-dashboard-home' : ' cuar-customer-dashboard-page';
+$content_class = $this->has_page_sidebar() ? $content_class . ' table-layout' : $content_class;
 ?>
-<div class="cuar-page <?php echo implode( ' ', $page_classes ); ?>">
-	<div class="cuar-page-header"><?php
-		$this->print_page_header( $args, $shortcode_content ); 
-	?></div>
-	
-	<div class="cuar-page-content<?php echo $content_classe; ?>"><?php
-		$this->print_page_content( $args, $shortcode_content ); 
-	?></div>
-	
-	<?php if ( $this->has_page_sidebar() ) : ?>	
-	<div class="cuar-page-sidebar"><?php 
-		$this->print_page_sidebar( $args, $shortcode_content ); 
-	?></div>	
-	<?php endif; ?>
-	
-	<div class="cuar-page-footer"><?php 
-		$this->print_page_footer( $args, $shortcode_content ); 
-	?></div>
+
+<div class="cuar-page clearfix <?php echo implode(' ', $page_classes); ?>">
+    <div class="cuar-page-header"><?php
+        $this->print_page_header($args, $shortcode_content);
+        ?></div>
+
+    <div class="cuar-page-content <?php echo $content_class; ?>">
+        <?php if ($this->has_page_sidebar()) { ?>
+            <div class="cuar-page-content-main tray tray-center va-t clearfix"><?php
+                $this->print_page_content($args, $shortcode_content);
+                ?></div>
+            <aside class="cuar-page-sidebar tray tray-right tray290 va-t clearfix">
+                <?php
+                $this->print_page_sidebar($args, $shortcode_content);
+                ?></aside>
+        <?php } else { ?>
+            <div class="cuar-page-content-main clearfix"><?php
+                $this->print_page_content($args, $shortcode_content);
+                ?></div>
+        <?php } ?>
+    </div>
+
+    <div class="cuar-page-footer clearfix"><?php
+        $this->print_page_footer($args, $shortcode_content);
+        ?></div>
 </div>
