a144b80e6fe076959977acdf90d040b213b63410
Vincent Mimoun-Prat <vpratfr@gmail.com>
2016-01-26 11:08:59 +0100

    Formatting

--- src/php/helpers/address-helper.class.php
+++ src/php/helpers/address-helper.class.php
@@ -46,7 +46,7 @@ if ( !class_exists('CUAR_AddressHelper')) :
         {
             if (!empty($address['vat-number'])) $address['vat_number'] = $address['vat-number'];
             if (!empty($address['logo-url'])) $address['logo_url'] = $address['logo-url'];
-            
+
             $address = array_merge(array(
                 'name'       => '',
                 'company'    => '',
