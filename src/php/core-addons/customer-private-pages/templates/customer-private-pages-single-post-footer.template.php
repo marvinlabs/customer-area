132c1228f283261b12f1e05d72bb7d7fe6a53afb
Vincent Mimoun-Prat <vpratfr@gmail.com>
2016-01-27 15:41:27 +0100

    Clean template

--- src/php/core-addons/customer-private-pages/templates/customer-private-pages-single-post-footer.template.php
+++ src/php/core-addons/customer-private-pages/templates/customer-private-pages-single-post-footer.template.php
@@ -28,45 +28,35 @@ $recipients = sprintf("<em>%s</em>", cuar_get_the_owner());
 
 
 <div class="cuar-single-post-footer<?php echo $extra_class; ?>">
-
-    <div class="row br-t pt20 mt5 clearfix">
-
+    <div class="row clearfix">
         <div class="cuar-author col-sm-4">
-            <div class="cuar-panel-meta-tile panel panel-tile">
+            <div class="panel panel-tile cuar-panel-meta-tile">
                 <div class="panel-body">
-                    <i class="fa fa-user icon-bg"></i>
-                    <h4 class="cuar-panel-meta-tile-value">
-                        <b><?php echo $author; ?></b>
-                    </h4>
-                    <h5 class="cuar-panel-meta-tile-property"><?php _e('Author', 'cuar'); ?></h5>
+                    <i class="fa fa-user"></i>
+                    <h4><?php echo $author; ?></h4>
+                    <h5><?php _e('Author', 'cuar'); ?></h5>
                 </div>
             </div>
         </div>
 
         <div class="cuar-owner col-sm-4">
-            <div class="cuar-panel-meta-tile panel panel-tile">
+            <div class="panel panel-tile cuar-panel-meta-tile">
                 <div class="panel-body">
-                    <i class="fa fa-users icon-bg"></i>
-                    <h4 class="cuar-panel-meta-tile-value">
-                        <b><?php echo $recipients; ?></b>
-                    </h4>
-                    <h5 class="cuar-panel-meta-tile-property"><?php _e('Recipient', 'cuar'); ?></h5>
+                    <i class="fa fa-users"></i>
+                    <h4><?php echo $recipients; ?></h4>
+                    <h5><?php _e('Recipient', 'cuar'); ?></h5>
                 </div>
             </div>
         </div>
 
         <div class="cuar-date col-sm-4">
-            <div class="cuar-panel-meta-tile panel panel-tile">
+            <div class="panel panel-tile cuar-panel-meta-tile">
                 <div class="panel-body">
-                    <i class="fa fa-calendar icon-bg"></i>
-                    <h4 class="cuar-panel-meta-tile-value">
-                        <b><?php echo $date; ?></b>
-                    </h4>
-                    <h5 class="cuar-panel-meta-tile-property"><?php _e('Date', 'cuar'); ?></h5>
+                    <i class="fa fa-calendar"></i>
+                    <h4><?php echo $date; ?></h4>
+                    <h5><?php _e('Date', 'cuar'); ?></h5>
                 </div>
             </div>
         </div>
-
     </div>
-
 </div>
