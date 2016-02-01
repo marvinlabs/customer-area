12620f9b505fdb43c979c9762a221507271087a7
Vincent Mimoun-Prat <vpratfr@gmail.com>
2016-02-01 21:40:07 +0100

    Task lists templates + remove extra info on other templates

--- src/php/core-addons/customer-private-files/templates/customer-private-files-content-item-container.template.php
+++ src/php/core-addons/customer-private-files/templates/customer-private-files-content-item-container.template.php
@@ -18,21 +18,7 @@
 ?>
 
 <?php
-global $post;
-
-$is_author = get_the_author_meta('ID') == get_current_user_id();
-if ($is_author)
-{
-    $subtitle_popup = __('You uploaded this file', 'cuar');
-    $subtitle = sprintf(__('Published for %s', 'cuar'), cuar_get_the_owner());
-}
-else
-{
-    $subtitle_popup = sprintf(__('Published for %s', 'cuar'), cuar_get_the_owner());
-    $subtitle = sprintf(__('Published by %s', 'cuar'), get_the_author_meta('display_name'));
-}
 $title_popup = sprintf(__('Uploaded on %s', 'cuar'), get_the_date());
-
 $file_count = cuar_get_the_attached_file_count($post->ID);
 ?>
 
@@ -40,10 +26,7 @@ $file_count = cuar_get_the_attached_file_count($post->ID);
     <td class="cuar-title">
         <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr($title_popup); ?>"><?php the_title(); ?></a>
     </td>
-    <td class="cuar-owner">
-        <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr($subtitle_popup); ?>"><?php echo $subtitle; ?></a>
-    </td>
-    <td class="cuar-file-count">
+    <td class="text-right cuar-file-count">
         <span class="label label-rounded label-default"><?php echo sprintf(_n('%1$s file', '%1$s files', $file_count, 'cuar'), $file_count); ?></span>
     </td>
 </tr>
--- src/php/core-addons/customer-private-pages/templates/customer-private-pages-content-item-container.template.php
+++ src/php/core-addons/customer-private-pages/templates/customer-private-pages-content-item-container.template.php
@@ -18,26 +18,11 @@
 ?>
 
 <?php
-$is_author = get_the_author_meta('ID') == get_current_user_id();
-if ($is_author)
-{
-    $subtitle_popup = __('You published this page', 'cuar');
-    $subtitle = sprintf(__('Published for %s', 'cuar'), cuar_get_the_owner());
-}
-else
-{
-    $subtitle_popup = sprintf(__('Published for %s', 'cuar'), cuar_get_the_owner());
-    $subtitle = sprintf(__('Published by %s', 'cuar'), get_the_author_meta('display_name'));
-}
-
 $title_popup = sprintf(__('Uploaded on %s', 'cuar'), get_the_date());
 ?>
 
 <tr>
     <td class="cuar-title">
         <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr($title_popup); ?>"><?php the_title(); ?></a>
-    </td>
-    <td class="cuar-owner">
-        <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr($subtitle_popup); ?>"><?php echo $subtitle; ?></a>
     </td>
 </tr>
