969a6d2c8fbbcf9f8c9cc8544e8aa957835b872c
Vincent Mimoun-Prat <vpratfr@gmail.com>
2016-02-01 17:34:55 +0100

    Better PHP

--- src/php/core-addons/customer-private-pages/templates/customer-private-pages-content-dashboard.template.php
+++ src/php/core-addons/customer-private-pages/templates/customer-private-pages-content-dashboard.template.php
@@ -2,23 +2,31 @@
 /** Template version: 3.0.0
  *
  * -= 3.0.0 =-
+ * - Improve UI for new master-skin
+ *
+ * -= 1.0.0 =-
  * - Initial version
  *
  */ ?>
 
+<?php /** @var string $page_subtitle */ ?>
+<?php /** @var string $all_items_url */ ?>
+<?php /** @var WP_Query $content_query */ ?>
+<?php /** @var string $item_template */ ?>
+
+<?php
+$all_items_url = cuar_addon('customer-private-pages')->get_page_url('customer-private-pages');
+?>
+
 <div class="cuar-collection panel panel-border panel-default top cuar_private_page">
     <div class="panel-heading">
-        <?php
-        $pp_addon = $this->plugin->get_addon('customer-private-pages');
-        $page_id = $pp_addon->get_page_id($this->get_slug());
-        ?>
         <span class="panel-icon">
             <i class="fa fa-book"></i>
         </span>
         <span class="panel-title">
             <?php echo $page_subtitle; ?>
             <span class="widget-menu pull-right">
-                <a href="<?php echo get_permalink($page_id); ?>" class="btn btn-default btn-xs">
+                <a href="<?php echo esc_attr($all_items_url); ?>" class="btn btn-default btn-xs">
                     <span class="fa fa-eye"></span> <?php _e('View all', 'cuar'); ?>
                 </a>
             </span>
