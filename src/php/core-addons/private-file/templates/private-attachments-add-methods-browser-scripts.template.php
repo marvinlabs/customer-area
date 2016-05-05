89ce1edfb0fbd46b04a3ad1f7d0e194d1f89af32
Vincent Mimoun-Prat <vpratfr@yahoo.fr>
2016-05-05 10:48:29 +0200

    Merge changes with  origin

--- src/php/core-addons/private-file/templates/private-attachments-add-methods-browser-scripts.template.php
+++ src/php/core-addons/private-file/templates/private-attachments-add-methods-browser-scripts.template.php
@@ -4,8 +4,21 @@
  */ ?>
 <script type="text/javascript">
     <!--
-    jQuery(document).ready(function ($) {
-        $('.cuar-js-file-attachment-manager').fileAttachmentManager();
-    });
+    "use strict";
+    (function ($) {
+        $(document).ready(function () {
+            if ($.cuar.isAdmin) {
+                // Init Select 2
+                cuarInitFileAttachmentManager();
+            } else {
+                // Wait for wizard to be initialized before Select 2
+                $('#cuar-js-content-container').on('cuar:wizard:initialized', cuarInitFileAttachmentManager);
+            }
+
+            function cuarInitFileAttachmentManager() {
+                $('.cuar-js-file-attachment-manager').fileAttachmentManager();
+            }
+        });
+    })(jQuery);
     //-->
 </script>
