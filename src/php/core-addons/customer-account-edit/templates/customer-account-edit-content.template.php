e684c332315ae251deea12f075aabfdb4decdeff
Vincent Mimoun-Prat <vpratfr@gmail.com>
2016-05-12 14:43:22 +0200

    Format code

--- src/php/core-addons/customer-account-edit/templates/customer-account-edit-content.template.php
+++ src/php/core-addons/customer-account-edit/templates/customer-account-edit-content.template.php
@@ -17,45 +17,44 @@ $current_user = get_userdata(get_current_user_id());
 
 <?php $this->print_form_header(); ?>
 
-<div class="page-heading">
-    <div class="media mb30 clearfix">
-        <div class="media-left pr30">
-            <?php echo get_avatar($current_user->ID); ?>
-        </div>
-        <div class="media-body va-m" style="width: 100%;">
-
-            <div class="cuar-title media-heading text-primary"><?php echo $current_user->display_name; ?>
-                <small> - Profile</small>
+    <div class="page-heading">
+        <div class="media mb30 clearfix">
+            <div class="media-left pr30">
+                <?php echo get_avatar($current_user->ID); ?>
             </div>
+            <div class="media-body va-m" style="width: 100%;">
 
-            <?php $this->print_submit_button(__('Submit', 'cuar')); ?>
+                <div class="cuar-title media-heading text-primary"><?php echo $current_user->display_name; ?>
+                    <small> - Profile</small>
+                </div>
 
+                <?php $this->print_submit_button(__('Submit', 'cuar')); ?>
+            </div>
+        </div>
+
+        <ul class="nav panel-tabs-border panel-tabs panel-tabs-left pl20 pr20">
+            <li class="active mn">
+                <a href="#cuar-js-account-edit-fields" data-toggle="tab" aria-expanded="true">
+                    <?php _e("General", "cuar"); ?>
+                </a>
+            </li>
+            <li class="mn">
+                <a href="#cuar-js-account-edit-address-fields" data-toggle="tab" aria-expanded="false">
+                    <?php _e("Addresses", "cuar"); ?>
+                </a>
+            </li>
+        </ul>
+
+    </div>
+    <div class="tab-content pn br-n">
+        <div id="cuar-js-account-edit-fields" class="tab-pane active">
+            <?php $this->print_account_fields(); ?>
+        </div>
+        <div id="cuar-js-account-edit-address-fields" class="tab-pane">
+            <div class="row">
+                <?php $this->print_address_fields(); ?>
+            </div>
         </div>
     </div>
-
-    <ul class="nav panel-tabs-border panel-tabs panel-tabs-left pl20 pr20">
-        <li class="active mn">
-            <a href="#cuar-js-account-edit-fields" data-toggle="tab" aria-expanded="true">
-                <?php _e("General", "cuar"); ?>
-            </a>
-        </li>
-        <li class="mn">
-            <a href="#cuar-js-account-edit-address-fields" data-toggle="tab" aria-expanded="false">
-                <?php _e("Addresses", "cuar"); ?>
-            </a>
-        </li>
-    </ul>
-
-</div>
-<div class="tab-content pn br-n">
-    <div id="cuar-js-account-edit-fields" class="tab-pane active">
-        <?php $this->print_account_fields(); ?>
-    </div>
-    <div id="cuar-js-account-edit-address-fields" class="tab-pane">
-        <div class="row">
-            <?php $this->print_address_fields(); ?>
-        </div>
-    </div>
-</div>
 
 <?php $this->print_form_footer(); ?>
