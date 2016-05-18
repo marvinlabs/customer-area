f697f72cf44c82c7f81195c5d528a8753f980aaa
Thomas Lartaud <contact@thomaslartaud.com>
2016-05-18 17:54:23 +0200

    tiny mce fixes

--- skins/frontend/master-dark/cuar-functions.php
+++ skins/frontend/master-dark/cuar-functions.php
@@ -11,12 +11,12 @@ if (!function_exists('cuar_custom_editor_styles')) {
         if (is_admin()) return $mce_css;
 
         if (cuar_is_customer_area_page(get_queried_object_id()) || cuar_is_customer_area_private_content(get_the_ID())) {
-            $mce_css .= ', ' . plugins_url('assets/css/custom-editor-style.css', __FILE__);
+            $mce_css = ',' . plugins_url('assets/css/styles.min.css', __FILE__);
         }
         return $mce_css;
     }
 
-    add_action('mce_css', 'cuar_custom_editor_styles');
+    add_filter('mce_css', 'cuar_custom_editor_styles');
 }
 
 // Includes functions from base theme
