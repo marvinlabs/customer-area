5c7f2cacc3ffcb12ec89ffc1e5adc68b102672f8
Thomas Lartaud <contact@thomaslartaud.com>
2016-06-01 16:49:02 +0200

    summernote options

--- src/js/frontend/summernote.js
+++ src/js/frontend/summernote.js
@@ -4,6 +4,14 @@ jQuery(document).ready(function($) {
 
     // Run summernote editor on the elements having that CSS class
     $(".cuar-js-richeditor").summernote({
-        // Put common SummerNote options here
+        toolbar: [
+            ['block', ['style']],
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['insert', ['table', 'hr', 'picture', 'link', 'video']],
+            ['view', ['codeview', 'fullscreen']],
+            ['tools', ['undo', 'redo', 'help']]
+        ]
     });
 });
