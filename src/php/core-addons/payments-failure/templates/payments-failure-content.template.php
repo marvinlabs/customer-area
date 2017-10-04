a42c35ea3feab3a932ab30dd6b5d5e6a1135d57f
Thomas Lartaud <contact@thomaslartaud.com>
2017-10-04 17:54:36 +0200

    Fix payments messages panels colors

--- src/php/core-addons/payments-failure/templates/payments-failure-content.template.php
+++ src/php/core-addons/payments-failure/templates/payments-failure-content.template.php
@@ -4,7 +4,7 @@
 $message = cuar_get_gateway_message();
 ?>
 
-<div class="alert alert-primary alert-success alert-border-left mb-lg">
+<div class="alert alert-danger alert-border-left mb-lg">
     <h3 class="mt-xs mb-lg"><?php _e('Sorry!', 'cuar') ?></h3>
     <p><?php _e('There has been a problem when recording your payment.', 'cuar') ?></p>
     <?php if (!empty($message)): ?>
