79e31896f976c5d5ea29d9a27d81934f040ae188
Thomas Lartaud <contact@thomaslartaud.com>
2018-01-08 16:38:45 +0100

    add new template so we can safely create disabled submit buttons

---
+++ src/php/core-classes/templates/edit-content-form-field-submit-disabled.template.php
@@ -0,0 +1,16 @@
+<?php
+/** Template version: 1.0.0
+ *
+ * -= 1.0.0 =-
+ * - Initial version
+ *
+ */ ?>
+
+<?php /** @var string $field_name */ ?>
+<?php /** @var string $field_label */ ?>
+
+<div class="form-group mn">
+    <div class="cuar-submit-container">
+        <input type="submit" value="<?php echo esc_attr($field_label); ?>" class="btn btn-default disabled" />
+    </div>
+</div>
